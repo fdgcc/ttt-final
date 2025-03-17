ec7a1835668466f9db2092e0d5a21abcc0a2661e
karthiga817 <22csec13@gmail.com>
2025-03-17 09:41:12 +0530

    testimonial

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import About from "./components/About";
 import Features from "./components/Features";
 import Story from "./components/Story";
 import Contact from "./components/Contact";
+import TestimonialSlider from "./components/test";
 import Footer from "./components/Footer";
 import Slider from "./components/slider";
 import Ahalya from "./fullstack/pages/ahalya";
@@ -19,36 +20,37 @@ import Shapna from "./fullstack/pages/shapna";
 import Varshini from "./fullstack/pages/varshini";
 import Vinothkumar from "./fullstack/pages/vinothkumar";
 import Naveenkumar from "./fullstack/pages/naveenkumaran";
+
 function App() {
   return (
     <Router>
       <main className="relative min-h-screen w-screen overflow-x-hidden">
         <NavBar />
         <Routes>
-          <Route path="/" element={
-            <>
-              <Hero />
-              <About />
-              <Slider />
-              
-              <Footer />
-            </>
-          } />
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <About />
+                <Slider />
+                <TestimonialSlider /> {/* Added TestimonialSlider here */}
+                <Footer />
+              </>
+            }
+          />
           <Route path="/fullstack" element={<EducationalPlatform />} />
-          <Route path="/rubypreethi" element={<Rubypreethi/>} />
+          <Route path="/rubypreethi" element={<Rubypreethi />} />
           <Route path="/ahalya" element={<Ahalya />} />
-          <Route path="/shekkina"element={<Shekkina />} />
-          <Route path="/balaji"element={<Balaji/>} />
-          <Route path="/harini"element={<Harini/>} />
-          <Route path="/vinoth"element={<Vinothkumar/>} />
-          <Route path="/naveenkumaran"element={<Naveenkumar/>} />
-          <Route path="/minojini"element={<Minojini/>} />
-          <Route path="/shapna"element={<Shapna/>} />
-          <Route path="/varshini"element={<Varshini/>} />
-          <Route path="/hebsiba"element={<Hebsiba/>} />
-
-
-          
+          <Route path="/shekkina" element={<Shekkina />} />
+          <Route path="/balaji" element={<Balaji />} />
+          <Route path="/harini" element={<Harini />} />
+          <Route path="/vinoth" element={<Vinothkumar />} />
+          <Route path="/naveenkumaran" element={<Naveenkumar />} />
+          <Route path="/minojini" element={<Minojini />} />
+          <Route path="/shapna" element={<Shapna />} />
+          <Route path="/varshini" element={<Varshini />} />
+          <Route path="/hebsiba" element={<Hebsiba />} />
         </Routes>
       </main>
     </Router>
