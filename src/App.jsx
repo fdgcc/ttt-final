dba7d9b2290d2c35e753cb0117c0bc82a094d1a0
fdgcc <rubipreethi2004@gmail.com>
2025-03-25 09:54:01 +0530

    routing added nd fs copmpleted

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,12 @@ import Shapna from "./fullstack/pages/shapna";
 import Varshini from "./fullstack/pages/varshini";
 import Vinothkumar from "./fullstack/pages/vinothkumar";
 import Naveenkumar from "./fullstack/pages/naveenkumaran";
-
+import App from "./components/About"
+import AnimatedTitle from "./components/AnimatedTitle";
 import ChatIcon from "./Chatbot/ChatIcon";
 
 
-function App() {
+function MainApp() {
 
   return (
     <Router>
@@ -43,6 +44,11 @@ function App() {
               </>
             }
           />
+          <Route path="/hierarchy" element={<About/>} />
+          <Route path="/testimonialslider" element={<TestimonialSlider/>} />
+          
+          <Route path="/slider" element={<Slider/>} />
+          <Route path="/slider" element={<Slider/>} />
           <Route path="/fullstack" element={<EducationalPlatform />} />
           <Route path="/rubypreethi" element={<Rubypreethi />} />
           <Route path="/ahalya" element={<Ahalya />} />
@@ -64,4 +70,4 @@ function App() {
   );
 }
 
-export default App;
+export default MainApp;
